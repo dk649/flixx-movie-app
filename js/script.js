67a88776a56eb4b53b2bde68c433787ef4d4c96a
dk649 <58308212+dk649@users.noreply.github.com>
2024-10-26 17:15:18 +0100

    updates
    added pagination

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: "",
     page: 1,
     totalPages: 1,
+    totalRsults: 0,
   },
   api: {
     apikey: "00a7fa5f1683c9a2219bedb30bcc6581",
@@ -48,6 +49,7 @@ async function displayPopularMovies() {
 
     document.querySelector("#popular-movies").appendChild(div);
   });
+  displayPagination();
 }
 
 // display most popular TV shows
@@ -287,7 +289,11 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if ((global.search.term !== "") & (global.search.term !== null)) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalRsults = total_results;
 
     if (results.length === 0) {
       showAlert("No Results", (className = "error"));
@@ -305,6 +311,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // clear prev results
+
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
   results.forEach((result) => {
     const div = document.createElement("div");
     div.classList.add("card");
@@ -345,10 +356,51 @@ function displaySearchResults(results) {
 
     `;
 
+    document.querySelector(
+      "#search-results-heading"
+    ).innerHTML = `<h2>${results.length} of ${global.search.totalRsults} Results for ${global.search.term}</h2>
+    `;
+
     document.querySelector("#search-results").appendChild(div);
   });
+
+  displayPagination();
 }
 
+//  pagination
+
+function displayPagination() {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = ` <button class="btn btn-primary" id="prev">Prev</button>
+          <button class="btn btn-primary" id="next">Next</button>
+          <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+        </div>`;
+
+  document.querySelector("#pagination").appendChild(div);
+
+  // disable prev
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+
+  // disable next on last page
+
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+}
 // Display Slider for movies
 
 async function displaySlider() {
@@ -461,7 +513,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
 
   const data = await response.json();
